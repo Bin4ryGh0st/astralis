e03b822fd3abb634e240c2047f9de6e0571c5530
AsitivaSinghal <astitvasinghal@gmail.com>
2018-10-28 10:16:59 +0530

    added moving bar
    red bar's width is changing

--- lev0.js
+++ lev0.js
@@ -1,4 +1,5 @@
 a=[[0,10,221,9],[75,40,200,9],[40,110,230,9],[75,10,15,129],[206,10,15,109],[256,80,15,70],[40,80,232,9]];
+b=[[40,80,232,9,232,9]];
 var canvas = document.getElementById("draw");
 var ctx = canvas.getContext("2d");
 var x= a[0][0]+4;
@@ -13,18 +14,35 @@ function drawWall()
 	{
 		ctx.beginPath();
 		ctx.rect(a[i][0],a[i][1],a[i][2],a[i][3]);
-		if(i==6 && attack)
-		{
-			ctx.fillStyle="#ff0000";
-		}
-		else
-		{
-			ctx.fillStyle="#ffffff";
-		}
-		ctx.fill();
+		ctx.fillStyle="#ffffff";
+        ctx.fill();b[0][0]
 		ctx.closePath();
 	}
+    ctx.beginPath();
+    ctx.rect(b[0][0],b[0][1],b[0][2],b[0][3]);
+    ctx.fillStyle="red";
+    ctx.fill();
+    ctx.closePath();
 	print(message);
+}
+function changeWidth()
+{
+    if( typeof changeWidth.c == 'undefined' ) {
+        changeWidth.c = 2;
+    }
+    if(b[0][2]+changeWidth.c>b[0][4] || b[0][2]+changeWidth.c<b[0][0] )
+    {
+        changeWidth.c=-changeWidth.c;
+    }
+    
+    ctx.clearRect(b[0][0],b[0][1],b[0][2],b[0][3]);
+    b[0][2]+=changeWidth.c;
+    ctx.beginPath();
+    ctx.rect(b[0][0],b[0][1],b[0][2],b[0][3]);
+    ctx.fillStyle="red";
+    ctx.fill();
+    ctx.closePath();
+    draw(0,0);
 }
 function drawBall()
 {
@@ -65,7 +83,10 @@ function draw(dx, dy)
         	}
         	console.log([x,y]);
     }
-    if(( x==84 || x==212) && (y==82 || y== 84))
+    for(var i=0;i<b.length;i++)
+    {
+        console.log([b[i][0],b[i][0]+b[i][2],b[i][1],b[i][1]+b[i][3]]);
+    if(( x>b[i][0] && x<b[i][0]+b[i][2]) && (y>b[i][1] && y<b[i][1]+b[i][3]))
     {
     	//alert("curropt");
     	attack=1;
@@ -76,10 +97,13 @@ function draw(dx, dy)
     	document.getElementById("unblockit").setAttribute("style","display: block;");
     	return;
     }
+    }
     drawWall();
     drawBall();
+    
     print(message);
 }
+
     function checkKeycode(event) {
     // handling Internet Explorer stupidity with window.event
     // @see http://stackoverflow.com/a/3985882/517705
@@ -103,4 +127,6 @@ function draw(dx, dy)
 
     return false;
     }
+    
   	document.onkeydown = checkKeycode;
+    setInterval(changeWidth, 10);
